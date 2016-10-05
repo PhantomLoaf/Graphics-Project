4f09ccb90466898150fdc5fa93a4b0c80efc2909
Ditzley <Ditzley278@yahoo.com>
2016-10-04 20:23:01 -0500

    Initial Commit

---
+++ Assets/Scripts/GenerateWorld.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateWorld : MonoBehaviour {
+    public int limit = 10;
+    public GameObject root;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < limit; i++)
+        {
+            GameObject go = Instantiate(root);
+            go.transform.position = new Vector3(i * 1.0f, 0, 0);
+            go.transform.rotation = Quaternion.identity;
+            go.transform.SetParent(root.transform);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Collider>().attachedRigidbody.useGravity = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * this.speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Physics.gravity = -Physics.gravity;
+        }
+    }
+}
