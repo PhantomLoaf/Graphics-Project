5062c33ae2fd6ac95fd794a08cba43caf6c52fbc
Ditzley <Ditzley278@yahoo.com>
2016-10-05 15:39:26 -0500

    Added loading screen scene and misc changes

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+    public Text scoreText;
+    public Text livesText;
+
+    private int score;
+ 	// Use this for initialization
+	void Start () {
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        score += (int)(100*Time.deltaTime);
+        scoreText.text = "Score: " + score;
+	}
+}
